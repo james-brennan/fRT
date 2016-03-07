a0edb8804676902621d637c3e75b5149ab79150a
Study Mac <janetbrennan@janetbrnansimac.home>
2016-03-07 22:50:28 +0000

    should do mc in parallel now

--- try1.py
+++ try1.py
@@ -52,10 +52,61 @@ def run():
     return gridd, rays
 
 
+def prun(x,y, maxs, gridd, scatt, red_nir_soil, bounding, lf):
+    rays = []
+    for i in xrange(50):
+        print (x,y)
+        r = ray(np.array([x,y,maxs-1]), np.array([1.0,0.0, -1.0]),
+                np.array([1.0, 1.0]))
+        r.dir /= np.linalg.norm(r.dir)
+        DDA_traversal(r,gridd, scatt, red_nir_soil, bounding, lf)
+        rays.append(r)
+    return rays
+
+
+
+
+def parallel_run():
+    """
+    Start parallel engines to run
+    """
+    from IPython.parallel import Client
+
+    c = Client()   # here is where the client establishes the connection
+    lv = c.load_balanced_view()   # this object represents the engines (workers)
+
+
+    rays = []
+    maxs=25
+    bounding = AABA(xmin=0, ymin=0, zmin=0, xmax=maxs, ymax=maxs, zmax=maxs,)
+    gridd = np.zeros((maxs,maxs,maxs))
+    # spectrum for red to nir leaves
+    red_nir_leaves = spectrum(np.array([0.5, 0.85]), np.array([0.1, 0.6]), np.array([0.5, 0.1]))
+    # spectrum for soil
+    red_nir_soil = spectrum(np.array([0.5, 0.85]), np.array([0.3, 0.4]), np.array([0.0, 0.0]))
+
+
+    # scattering setup
+    scatt = BRDSF(red_nir_leaves, 0.0)
+    lf = leaf(55.0, 0.8) # leaf angle distribution and leaf area density
+
+
+    tasks = []
+    for x in xrange(maxs):
+        for y in xrange(maxs):
+            tasks.append(lv.apply(prun, x,y, maxs, gridd, scatt, red_nir_soil, bounding, lf))
+
+    result = [task.get() for task in tasks]  # blocks until all results are back
+
+    return results
+
+
+
+
 import pylab as plt
 
-gri = run()
-
+#gri = run()
+pgri = parallel_run()
 plt.imshow(gri[0].sum(axis=2)); plt.show()
 
 
